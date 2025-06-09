e021fdbf5c3196edd0bd2a580046d8329d13960c
ahmed mohamed <ahmudmohamed78s@gmail.com>
2025-06-09 17:03:20 +0300

    update verify email controller

--- src/Modules/User/user.controller.js
+++ src/Modules/User/user.controller.js
@@ -94,8 +94,9 @@ export const verifyEmail = async (req, res, next) => {
         );
     }
     // response
-    res.status(200).json({ message: "User email successfully confirmed you can login now" });
-    // res.status(200).json({ message: "User email successfully confirmed you can login now" });
+     // Redirect to your Vercel frontend login page with optional message
+    res.redirect('https://brain-tumor-frontend-five.vercel.app/login');
+    //res.status(200).json({ message: "User email successfully confirmed you can login now" });
 };
 
 /***
